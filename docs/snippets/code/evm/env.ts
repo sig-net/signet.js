4645d5b611c2f9445b01fec8142faf8b2ed9c2e3
Pessina <fs.pessina@gmail.com>
2025-02-28 18:56:44 -0300

    Clean up

--- docs/snippets/code/evm/env.ts
+++ docs/snippets/code/evm/env.ts
@@ -2,13 +2,7 @@ import { createPublicClient, createWalletClient, http } from 'viem'
 import { privateKeyToAccount } from 'viem/accounts'
 import { sepolia } from 'viem/chains'
 
-const privateKey = process.env.PRIVATE_KEY as `0x${string}`
-
-if (!privateKey) {
-  throw new Error('PRIVATE_KEY must be set in environment')
-}
-
-const account = privateKeyToAccount(privateKey)
+const account = privateKeyToAccount(process.env.PRIVATE_KEY as `0x${string}`)
 
 const publicClient = createPublicClient({
   chain: sepolia,
