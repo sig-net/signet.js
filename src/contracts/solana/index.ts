1879bec68af9f9a0d7ecbbff172c406c7ef284e1
Osman Abdelnasir <esaminu1@icloud.com>
2025-04-08 02:09:56 +0400

    feat: add exports

---
+++ src/contracts/solana/index.ts
@@ -0,0 +1,10 @@
+import * as ChainSignaturesContractIdl from './types/chain_signatures_project.json'
+import * as errors from './errors'
+export * from './ChainSignaturesContract'
+
+const utils = {
+  ChainSignaturesContractIdl,
+  errors,
+}
+
+export { utils }
